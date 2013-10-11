3696ce881380b572814aabef6e713e89044b8e15
Gavin Joyce <gavinjoyce@gmail.com>
2013-10-11 21:32:30 +0100

    moved Email::Send command to restpack_service_messaging

--- spec/commands/email/send_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Commands::Email::Send do
-  is_required :application_id, :from, :to, :subject, :text_body
-  is_optional :cc, :bcc, :reply_to, :html_body
-
-  pending 'TODO: add some specs'
-end
