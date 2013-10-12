1817a7c6e1a0dfd60f25bf090a339ed12eadd0b7
Gavin Joyce <gavinjoyce@gmail.com>
2013-10-12 20:09:49 +0100

    added support for email header and footers

--- lib/restpack_email_service/jobs/email/send.rb
+++ lib/restpack_email_service/jobs/email/send.rb
@@ -21,9 +21,16 @@ module RestPack::Email::Service::Jobs
       private
 
       def get_template(params)
-        identifier = params['template']
+        application_id = params['application_id']
+        template = resolve_template(application_id, params['template'])
+        inject_header_and_footer(template, application_id)
+
+        template
+      end
+
+      def resolve_template(application_id, identifier)
         template = Models::EmailTemplate.where(
-          application_id: params['application_id'],
+          application_id: application_id,
           identifier: identifier
         ).take
 
@@ -45,6 +52,23 @@ module RestPack::Email::Service::Jobs
         filepath = File.expand_path("#{template_path}/#{identifier}.#{format}.liquid", __FILE__)
         if File.exists? filepath
           return File.read(filepath)
+        end
+      end
+
+      def inject_header_and_footer(template, application_id)
+        if template
+          header = resolve_template(application_id, '_header')
+          footer = resolve_template(application_id, '_footer')
+
+          if template.html_template
+            template.html_template.gsub! '{{header}}', header.html_template
+            template.html_template.gsub! '{{footer}}', footer.html_template
+          end
+
+          if template.text_template
+            template.text_template.gsub! '{{header}}', header.text_template
+            template.text_template.gsub! '{{footer}}', footer.text_template
+          end
         end
       end
     end
