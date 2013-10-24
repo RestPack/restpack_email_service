d87d8ae349e0c763618555411136fb2f0a29f697
Gavin Joyce <gavinjoyce@gmail.com>
2013-10-24 12:40:15 +0100

    send_raw job updated to support new db schema

--- lib/restpack_email_service/jobs/email/send_raw.rb
+++ lib/restpack_email_service/jobs/email/send_raw.rb
@@ -7,32 +7,32 @@ module RestPack::Email::Service::Jobs
       include Sidekiq::Worker
 
       def perform(params)
+        params.deep_symbolize_keys!
         settings = email_settings(params)
 
-        #TODO: GJ: add support for providers other than AWS SES
         Mail.defaults do
           delivery_method :smtp, {
-            :address              => 'email-smtp.us-east-1.amazonaws.com',
-            :port                 => 587,
-            :user_name            => settings[:username],
-            :password             => settings[:secret],
-            :enable_starttls_auto => true
+            address:              settings[:smtp_server],
+            port:                 settings[:smtp_port],
+            user_name:            settings[:smtp_username],
+            password:             settings[:smtp_password],
+            enable_starttls_auto: true
           }
         end
 
         mail = Mail.new do
-          to params['to']
-          from params['from']
-          subject params['subject']
+          to params[:to] || settings[:default_from]
+          from params[:from]
+          subject params[:subject]
 
           text_part do
-            body params['text_body']
+            body params[:text_body]
           end
 
-          if params['html_body']
+          if params[:html_body]
             html_part do
               content_type 'text/html; charset=UTF-8'
-              body params['html_body']
+              body params[:html_body]
             end
           end
         end
@@ -42,13 +42,12 @@ module RestPack::Email::Service::Jobs
 
       private
 
-      def email_settings(params) #TODO: GJ: use models to fetch the relevant email settings
+      def email_settings(params)
         settings = Serializers::EmailSetting.resource(
-          application_id: params['application_id']
+          application_id: params[:application_id]
         )[:settings].first
 
         raise "Email is not setup for this application" unless settings
-        raise "Only the Amazon SES provider is supported" unless settings[:provider] == 'aws_ses'
         return settings
       end
     end
